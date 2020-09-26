e2b94cb36892b0e432fbc71e26a72f9a97113685
Dzulfanni <belajarayok866@gmail.com>
2020-09-26 14:32:48 +0700

    first commit

---
+++ no3.js
@@ -0,0 +1,15 @@
+var p = 5;
+var l = 6;
+
+
+function hitungLuasPersegiPanjang(p,l){
+ 
+    total= p*l;
+    return total;
+    
+}
+hitungLuasPersegiPanjang(p,l);
+
+document.write('<br>'+'Panjang:' + p + '</br>');
+document.write('Lebar:'+ l + '</br>');
+document.write('Luas persegi panjang: ' + total )
